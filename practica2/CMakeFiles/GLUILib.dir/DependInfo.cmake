1031068560eecc50e4f4d23fb97e61d5c416997e
Valentino Lugli <valentinolugli@gmail.com>
2021-04-09 11:23:04 +0200

    [P2][0%] Practice 2 Initial Push

---
+++ practica2/CMakeFiles/GLUILib.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/algebra3.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/algebra3.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/arcball.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/arcball.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_add_controls.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_add_controls.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_bitmap_img_data.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_bitmap_img_data.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_bitmaps.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_bitmaps.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_button.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_button.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_checkbox.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_checkbox.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_column.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_column.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_commandline.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_commandline.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_control.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_control.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_edittext.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_edittext.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_filebrowser.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_filebrowser.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_list.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_list.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_listbox.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_listbox.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_mouse_iaction.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_mouse_iaction.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_node.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_node.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_panel.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_panel.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_radio.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_radio.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_rollout.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_rollout.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_rotation.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_rotation.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_scrollbar.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_scrollbar.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_separator.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_separator.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_spinner.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_spinner.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_statictext.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_statictext.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_string.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_string.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_textbox.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_textbox.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_translation.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_translation.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_tree.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_tree.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_treepanel.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_treepanel.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/glui_window.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/glui_window.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/quaternion.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/quaternion.cpp.o"
+  "/home/valentino/Desktop/Git/ia2021/practica2/src/glui/viewmodel.cpp" "/home/valentino/Desktop/Git/ia2021/practica2/CMakeFiles/GLUILib.dir/src/glui/viewmodel.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
